57ac5337a9fa597d686194176dd0a97d7b08fd7c
Victor Jian Choe <vchoe89@gmail.com>
2019-07-28 22:36:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,7 @@ def summon_captain_planet(planateer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls()
 end
 
 def find_the_cheese# code an argument here
