f4a35a4d4b5d033440ba6c632d04ef95472f03b1
Victor Jian Choe <vchoe89@gmail.com>
2019-07-28 22:40:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,5 @@ def find_the_cheese(array)
   i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
   while i < cheese_types.length do
-    return cheese_types[i] if array.include?
+    return cheese_types[i] if array.include?(cheese_types[i])
 end
